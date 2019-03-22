09cb1664c91cda3de75e0f98ea672f0edb9a59e8
Ivan Čurić <ivan.curic@gmail.com>
2019-03-22 10:30:38 +0100

    Caching of post

--- src/components/PostFull.js
+++ src/components/PostFull.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import { set, get } from 'money-clip';
 import { getData } from '../getData';
 
 function PostFull(props) {
   const postId = props.match.params.id;
-  const [post, setData] = useState({
+  const [post, setPost] = useState({
     title: '',
     body: '',
     bodyArray: [],
@@ -11,11 +12,19 @@ function PostFull(props) {
 
   useEffect(() => {
     async function fetchData() {
+      const cachedPost = await get(`post_id_${postId}`);
+
+      if (cachedPost) {
+        setPost(cachedPost);
+        return;
+      }
+
       const post = await getData('posts/' + postId);
       const bodyArray = post.data.body.split('\n');
-      setData({ ...post.data, bodyArray });
+      const postWithArray = { ...post.data, bodyArray };
+      set(`post_id_${postId}`, postWithArray);
+      setPost(postWithArray);
     }
-
     fetchData();
   }, []);
 
